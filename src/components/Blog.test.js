4dbf56c3608592137235431e956ca4dcc36eba57
Jesse Toivanen <jesse.toivanen2@gmail.com>
2022-04-04 11:02:28 +0300

    Add blog render test for show button, 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,31 +1,47 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 import App from '../App'
 
-test('renders only title and author by default', () => {
-  const blog = {
-    title: 'Test Engineering',
-    author: 'Gerhard Berger',
-    url: 'http://greatblogs.com/gberger/testing',
-    likes: 234,
-    user: {
-      username: 'mluukkai',
-      name: 'Matti Luukkainen'
-    }
-  }
-
-  const user = {
+const blog = {
+  title: 'Test Engineering',
+  author: 'Gerhard Berger',
+  url: 'http://greatblogs.com/gberger/testing',
+  likes: 234,
+  user: {
     username: 'mluukkai',
     name: 'Matti Luukkainen'
   }
+}
 
-  const { container } = render(<Blog blog={blog} user={user} handleAddLike={() => App.handleAddLike} handleRemoveBlog={() => App.handleRemoveBlog} />)
+const user = {
+  username: 'mluukkai',
+  name: 'Matti Luukkainen'
+}
+
+test('renders only title and author by default', () => {
+  const { container } = render(<Blog blog={blog} user={user} handleAddLike={() =>
+    App.handleAddLike} handleRemoveBlog={() => App.handleRemoveBlog} />)
 
   const lessInfo = container.querySelector('.lessInfo')
   const moreInfo = container.querySelector('.moreInfo')
 
   expect(lessInfo).toBeVisible()
   expect(moreInfo).not.toBeVisible()
+})
+
+test('renders all info after "show" button has been pressed', () => {
+  const { container } = render(<Blog blog={blog} user={user} handleAddLike={() =>
+    App.handleAddLike} handleRemoveBlog={() => App.handleRemoveBlog} />)
+
+  const button = screen.getByText('view')
+  userEvent.click(button)
+
+  const lessInfo = container.querySelector('.lessInfo')
+  const moreInfo = container.querySelector('.moreInfo')
+
+  expect(lessInfo).not.toBeVisible()
+  expect(moreInfo).toBeVisible()
 })
